0f74c5efce43c5fc67af5d6d911dba3a84f143a7
Alexei Shashev <ashashev@gmail.com>
2014-12-02 03:13:30 +0400

    Неправильно отображается название месяца в отчете за неделю
    closes #193 @2

--- dm.pas
+++ dm.pas
@@ -214,7 +214,14 @@ procedure TdmMain.sqlWeekReportmonthGetText(Sender: TField; var aText: string;
   DisplayText: Boolean);
 begin
   if not sqlWeekReportMonth.IsNull then
-    aText := FormatDateTime('mmm',StrToDate(sqlWeekReportMonth.Value,'m'))
+    aText := SysToUTF8( FormatDateTime(
+           'mmm',
+           StrToDate(
+             sqlWeekReportMonth.Value,
+             dbDateFormatStr,
+             dbDateSeparator
+             )
+           ))
   else
     aText := '';
 end;
